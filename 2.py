deb2788627e6acd777944a56e735f07e9345ff45
mihir hadavani <miheerhadvani990@gmail.com>
2022-04-24 13:32:11 +0530

    my

---
+++ 2.py
@@ -0,0 +1,34 @@
+str1 = "My name is Mihir"
+str2 = "I am from IT department"
+print(str1)
+a = 68
+b =49.345
+
+#print(type(a))   # type of vriable
+
+print(a+b)
+
+c = "23"
+d ="67"
+
+print("type cast char to int", int(c) + int(d), "type int 68 to char ", str(a))
+
+print(10* "Hello world\n")
+
+#for int 10 muliply thats why to type cast to str
+
+print(10 * str(a+b))
+
+print("Enter the number :")
+num = input()  # input as a string for int type cast required
+
+print("You enter this number :", int(num))
+
+print("Enter the first number :")
+m = input()
+print("Enter the second number :")
+n = input()
+
+print("Sum of number is :", int(m) + int(n))
+
+
---
+++ 4.py
@@ -0,0 +1,21 @@
+item = ["c/c++", "java", "python", 56]
+
+print(item)
+print(item[0])
+num = [1,2,3,4,5,6,7, 99, 667, 24]
+
+print(num)
+num.sort()
+print(num)
+print(num[3])
+num.reverse()
+print("Reverse number ", num)
+print(num[2:5])
+print("length of number ", len(num))
+print("maximum of these number ", max(num))
+print("minimum of these number ", min(num))
+print("before append", num)
+num.append(86)  # 86 at last
+print(num)
+
+
