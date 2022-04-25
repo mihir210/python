8e8ccd96e463c2c3c6939aa396c75e94b4af6a58
mihir hadavani <miheerhadvani990@gmail.com>
2022-04-25 09:25:42 +0530

    first

--- tur1.py
+++ tur1.py
@@ -1,305 +1,155 @@
-from turtle import *
+# import required modules
 import turtle
+import time
+import random
+
+delay = 0.1
+score = 0
+high_score = 0
+
+# Creating a window screen
 wn = turtle.Screen()
-wn.setup(width=1000, height=800)
+wn.title("Snake Game")
+wn.bgcolor("blue")
+# the width and height can be put as user's choice
+wn.setup(width=600, height=600)
+wn.tracer(0)
 
-def my_goto(x, y):
-    penup()
-    goto(x, y)
-    pendown()
+# head of the snake
+head = turtle.Turtle()
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "Stop"
 
+# food in the game
+food = turtle.Turtle()
+colors = random.choice(['red', 'green', 'black'])
+shapes = random.choice(['square', 'triangle', 'circle'])
+food.speed(0)
+food.shape(shapes)
+food.color(colors)
+food.penup()
+food.goto(0, 100)
 
-def eyes():
-    fillcolor("#ffffff")
-    begin_fill()
-
-    tracer(False)
-    a = 2.5
-    for i in range(120):
-        if 0 <= i < 30 or 60 <= i < 90:
-            a -= 0.05
-            lt(3)
-            fd(a)
-        else:
-            a += 0.05
-            lt(3)
-            fd(a)
-    tracer(True)
-    end_fill()
-
-
-
-def beard():
-    my_goto(-32, 135)
-    seth(165)
-    fd(60)
-
-    my_goto(-32, 125)
-    seth(180)
-    fd(60)
-
-    my_goto(-32, 115)
-    seth(193)
-    fd(60)
-
-    my_goto(37, 135)
-    seth(15)
-    fd(60)
-
-    my_goto(37, 125)
-    seth(0)
-    fd(60)
-
-    my_goto(37, 115)
-    seth(-13)
-    fd(60)
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 250)
+pen.write("Score : 0 High Score : 0", align="center",
+          font=("candara", 24, "bold"))
 
 
-def mouth():
-    my_goto(5, 148)
-    seth(270)
-    fd(100)
-    seth(0)
-    circle(120, 50)
-    seth(230)
-    circle(-120, 100)
+# assigning key directions
+def group():
+    if head.direction != "down":
+        head.direction = "up"
 
 
-def scarf():
-    fillcolor('#e70010')
-    begin_fill()
-    seth(0)
-    fd(200)
-    circle(-5, 90)
-    fd(10)
-    circle(-5, 90)
-    fd(207)
-    circle(-5, 90)
-    fd(10)
-    circle(-5, 90)
-    end_fill()
+def godown():
+    if head.direction != "up":
+        head.direction = "down"
 
 
-def nose():
-    my_goto(-10, 158)
-    seth(315)
-    fillcolor('#e70010')
-    begin_fill()
-    circle(20)
-    end_fill()
+def goleft():
+    if head.direction != "right":
+        head.direction = "left"
 
 
-def black_eyes():
-    seth(0)
-    my_goto(-20, 195)
-    fillcolor('#000000')
-    begin_fill()
-    circle(13)
-    end_fill()
-
-    pensize(6)
-    my_goto(20, 205)
-    seth(75)
-    circle(-10, 150)
-    pensize(3)
-
-    my_goto(-17, 200)
-    seth(0)
-    fillcolor('#ffffff')
-    begin_fill()
-    circle(5)
-    end_fill()
-    my_goto(0, 0)
+def goright():
+    if head.direction != "left":
+        head.direction = "right"
 
 
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
 
 
-def face():
+wn.listen()
+wn.onkeypress(group, "w")
+wn.onkeypress(godown, "s")
+wn.onkeypress(goleft, "a")
+wn.onkeypress(goright, "d")
 
-    fd(183)
-    lt(45)
-    fillcolor('#ffffff')
-    begin_fill()
-    circle(120, 100)
-    seth(180)
-    # print(pos())
-    fd(121)
-    pendown()
-    seth(215)
-    circle(120, 100)
-    end_fill()
-    my_goto(63.56,218.24)
-    seth(90)
-    eyes()
-    seth(180)
-    penup()
-    fd(60)
-    pendown()
-    seth(90)
-    eyes()
-    penup()
-    seth(180)
-    fd(64)
+segments = []
 
+# Main Gameplay
+while True:
+    wn.update()
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "Stop"
+        colors = random.choice(['red', 'blue', 'green'])
+        shapes = random.choice(['square', 'circle'])
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segments.clear()
+        score = 0
+        delay = 0.1
+        pen.clear()
+        pen.write("Score : {} High Score : {} ".format(
+            score, high_score), align="center", font=("candara", 24, "bold"))
+    if head.distance(food) < 20:
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
+        food.goto(x, y)
 
-def head():
-    penup()
-    circle(150, 40)
-    pendown()
-    fillcolor('#00a0de')
-    begin_fill()
-    circle(150, 280)
-    end_fill()
+        # Adding segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("orange")  # tail colour
+        new_segment.penup()
+        segments.append(new_segment)
+        delay -= 0.001
+        score += 10
+        if score > high_score:
+            high_score = score
+        pen.clear()
+        pen.write("Score : {} High Score : {} ".format(
+            score, high_score), align="center", font=("candara", 24, "bold"))
+    # Checking for head collisions with body segments
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+    move()
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+            colors = random.choice(['red', 'blue', 'green'])
+            shapes = random.choice(['square', 'circle'])
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segment.clear()
 
+            score = 0
+            delay = 0.1
+            pen.clear()
+            pen.write("Score : {} High Score : {} ".format(
+                score, high_score), align="center", font=("candara", 24, "bold"))
+    time.sleep(delay)
 
-def Doraemon():
-    head()
-    scarf()
-    face()
-    black_eyes()
-    nose()
-    mouth()
-    beard()
-    my_goto(0, 0)
-    seth(0)
-    penup()
-    circle(150, 50)
-    pendown()
-    seth(30)
-    fd(40)
-    seth(70)
-    circle(-30, 270)
-
-
-    fillcolor('#00a0de')
-    begin_fill()
-
-    seth(230)
-    fd(80)
-    seth(90)
-    circle(1000, 1)
-    seth(-89)
-    circle(-1000, 10)
-
-    # print(pos())
-
-    seth(180)
-    fd(70)
-    seth(90)
-    circle(30, 180)
-    seth(180)
-    fd(70)
-
-    # print(pos())
-    seth(100)
-    circle(-1000, 9)
-
-    seth(-86)
-    circle(1000, 2)
-    seth(230)
-    fd(40)
-
-    # print(pos())
-
-
-    circle(-30, 230)
-    seth(45)
-    fd(81)
-    seth(0)
-    fd(203)
-    circle(5, 90)
-    fd(10)
-    circle(5, 90)
-    fd(7)
-    seth(40)
-    circle(150, 10)
-    seth(30)
-    fd(40)
-    end_fill()
-
-    seth(70)
-    fillcolor('#ffffff')
-    begin_fill()
-    circle(-30)
-    end_fill()
-
-    my_goto(103.74, -182.59)
-    seth(0)
-    fillcolor('#ffffff')
-    begin_fill()
-    fd(15)
-    circle(-15, 180)
-    fd(90)
-    circle(-15, 180)
-    fd(10)
-    end_fill()
-
-    my_goto(-96.26, -182.59)
-    seth(180)
-    fillcolor('#ffffff')
-    begin_fill()
-    fd(15)
-    circle(15, 180)
-    fd(90)
-    circle(15, 180)
-    fd(10)
-    end_fill()
-
-    my_goto(-133.97, -91.81)
-    seth(50)
-    fillcolor('#ffffff')
-    begin_fill()
-    circle(30)
-    end_fill()
-
-    my_goto(-103.42, 15.09)
-    seth(0)
-    fd(38)
-    seth(230)
-    begin_fill()
-    circle(90, 260)
-    end_fill()
-
-    my_goto(5, -40)
-    seth(0)
-    fd(70)
-    seth(-90)
-    circle(-70, 180)
-    seth(0)
-    fd(70)
-
-    my_goto(-103.42, 15.09)
-    fd(90)
-    seth(70)
-    fillcolor('#ffd200')
-    # print(pos())
-    begin_fill()
-    circle(-20)
-    end_fill()
-    seth(170)
-    fillcolor('#ffd200')
-    begin_fill()
-    circle(-2, 180)
-    seth(10)
-    circle(-100, 22)
-    circle(-2, 180)
-    seth(180-10)
-    circle(100, 22)
-    end_fill()
-    goto(-13.42, 15.09)
-    seth(250)
-    circle(20, 110)
-    seth(90)
-    fd(15)
-    dot(10)
-    my_goto(0, -150)
-
-
-
-if __name__ == '__main__':
-    pensize(3)
-    speed(9)
-    Doraemon()
-    my_goto(100, -300)
-    write('@python.for.fun', font=("Bradley Hand ITC", 30, "bold"))
-    mainloop()
+wn.mainloop()
