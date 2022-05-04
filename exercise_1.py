4a809fea38a4044ba73662c220d9f219d0bc7f23
mihir hadavani <miheerhadvani990@gmail.com>
2022-05-04 22:53:50 +0530

    first

--- exercise_1.py
+++ exercise_1.py
@@ -1,4 +1,31 @@
-d2 = {"set":"a group of words","mutable":"that can change ","mortal":"which can die","immutable":"which cannot change"}
-var1 = str(input())
-if var1 in d2:
-    print(d2[var1])
+import cv2
+import os
+
+cascPath=os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frames = video_capture.read()
+
+    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frames)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
