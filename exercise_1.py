32d33fe8d8e9efc076cc85d52cec9a4ed6e24975
mihir hadavani <miheerhadvani990@gmail.com>
2022-06-22 21:54:25 +0530

    first

--- exercise_1.py
+++ exercise_1.py
@@ -1,31 +1,47 @@
-import cv2
-import os
+# Creating database
+# It captures images and stores them in datasets
+# folder under the folder name of sub_data
+import cv2, sys, numpy, os
+haar_file = 'haarcascade_frontalface_default.xml'
 
-cascPath=os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
+# All the faces data will be
+# present this folder
+datasets = 'datasets'
 
-video_capture = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frames = video_capture.read()
+# These are sub data sets of folder,
+# for my faces I've used my name you can
+# change the label here
+sub_data = 'vivek'	
 
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+path = os.path.join(datasets, sub_data)
+if not os.path.isdir(path):
+	os.mkdir(path)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+# defining the size of images
+(width, height) = (130, 100)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#'0' is used for my webcam,
+# if you've any other camera
+# attached use '1' like this
+face_cascade = cv2.CascadeClassifier(haar_file)
+webcam = cv2.VideoCapture(0)
 
-    # Display the resulting frame
-    cv2.imshow('Video', frames)
+# The program loops until it has 30 images of the face.
+count = 1
+while count < 30:
+	(_, im) = webcam.read()
+	gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+	for (x, y, w, h) in faces:
+		cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2)
+		face = gray[y:y + h, x:x + w]
+		face_resize = cv2.resize(face, (width, height))
+		cv2.imwrite('% s/% s.png' % (path, count), face_resize)
+	count += 1
+	
+	cv2.imshow('OpenCV', im)
+	key = cv2.waitKey(10)
+	if key == 27:
+		break
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
