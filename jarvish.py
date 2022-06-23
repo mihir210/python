d1077f396d6bc8b035451748cdd7b895bcf1b2b2
mihir hadavani <miheerhadvani990@gmail.com>
2022-06-23 20:21:38 +0530

    first

--- jarvish.py
+++ jarvish.py
@@ -8,7 +8,7 @@ import  webbrowser
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[2].id)
 #print(voices[1].id)
 def speak(audio):
     engine.say(audio)
@@ -24,6 +24,16 @@ def wishme():
         speak("good evening")
 
     speak("I am Jarvis. please tell me how may I help you")
+def ini():
+    ss = sr.Recognizer()
+    with sr.Microphone() as source:
+        au = ss.listen(source)
+    try:
+        sd = ss.recognize_google(au, language='en-in')
+    except Exception as e:
+        return "none"
+    return sd
+
 
 def takecommand():
     r = sr.Recognizer()
@@ -41,36 +51,40 @@ def takecommand():
         #print(e)
         print("say that again please...")
         speak("say that again please...")
+
         return "none"
     return query
 if __name__ == '__main__':
     wishme()
     while True:
-        query = takecommand().lower()
+        ss = ini().lower()
 
-        if 'wikipedia' in query:
-            speak("searching Wikipedia...")
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=1)
-            speak("According to wikipedia")
-            print(results)
-            speak(results)
+        if 'google' in ss:
+            query = takecommand().lower()
+            if 'wikipedia' in query:
+                speak("searching Wikipedia...")
+                query = query.replace("wikipedia", "")
+                results = wikipedia.summary(query, sentences=1)
+                speak("According to wikipedia")
+                print(results)
+                speak(results)
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
-        elif 'open chrome' in query:
-            cpath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-            os.startfile(cpath)
-        elif 'open google' in query:
-            speak("opening google")
-            webbrowser.open("google.com")
-        elif 'the time' in query:
-            strtime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"the time is {strtime}")
-        elif 'open whatsapp' in query:
-            wpath = "C:\\Users\\Mihir\\AppData\\Local\\WhatsApp\\WhatsApp.exe"
-            speak("Opening Whatsapp")
-            os.startfile(wpath)
-        elif 'exit' in query:
-            speak("good by, have a nice day")
-            exit(0)
+            elif 'open youtube' in query:
+                webbrowser.open("youtube.com")
+            elif 'open chrome' in query:
+                cpath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+                os.startfile(cpath)
+                speak("opening youtube")
+            elif 'open google' in query:
+                speak("opening google")
+                webbrowser.open("google.com")
+            elif 'the time' in query:
+                strtime = datetime.datetime.now().strftime("%H:%M:%S")
+                speak(f"the time is {strtime}")
+            elif 'open whatsapp' in query:
+                wpath = "C:\\Users\\Mihir\\AppData\\Local\\WhatsApp\\WhatsApp.exe"
+                speak("Opening Whatsapp")
+                os.startfile(wpath)
+            elif 'exit' in query:
+                speak("good by, have a nice day")
+                exit(0)
