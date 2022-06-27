b806afb780a2da19cfc1de6978fba3106cc4a9b2
mihir hadavani <miheerhadvani990@gmail.com>
2022-06-27 17:18:09 +0530

    first

--- first_AI_jarvish/jarvish.py
+++ first_AI_jarvish/jarvish.py
@@ -8,7 +8,7 @@ import webbrowser
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[2].id)
+engine.setProperty('voice', voices[0].id)
 #print(voices[1].id)
 def speak(audio):
     engine.say(audio)
